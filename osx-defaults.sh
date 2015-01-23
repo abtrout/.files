39650116853af6d6ea0c54a5c529b8b38809e9e1
david cochran <about.trout@gmail.com>
2015-01-23 10:47:34 -0600

    stash work on osx-defaults

---
+++ osx-defaults.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+# My OS X default/tweaks, updated for Yosemite. See hints.macworld.com and mths.be/osx for more!
+
+sudo -v
+
+# Set hostname if provided as argument
+if [[ $# -eq 1 ]]; then
+  scutil --set ComputerName $1
+  scutil --set HostName $1
+  scutil --set LocalHostName $1
+  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$1"
+fi
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Skip verifying disk images.
+defaults write com.apple.frameworks.DiskImages skip-verify -bool true
+defaults write com.apple.frameworks.DiskImages skip-verify-locked -bool true
+defaults write com.apple.frameworks.DiskImages skip-verify-remote -bool true
+
+# Don't ask to use Time Machine and disable local backups.
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+## Disable Safari webpage previews and enabled Debug menu. 
+#chflags uchg ~/Library/Caches/com.apple.Safari/Webpage\ Previews/
+#defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+#defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Don't ask if we're sure we want to open an application we just double clicked on... 
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Don't write .DS_Stores in network volumes
+defaults write com.apple.DesktopServices DSDontWriteNetworkStores -bool true
+
+# Speed up Save sheet animations
+defaults write -g NSWindowResizeTime -float 0.001
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Disable smart quotes, smart dashes, and automatic spelling correction
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
+defaults write -g AppleFontSmoothing -int 1
+
+#defaults write com.apple.Dock mcx-expose-disabled -bool true
+defaults write com.apple.Dock expose-animation-duration -float 0
+defaults write com.apple.Dock autohide-delay -float 0
+defaults write com.apple.dashboard mcx-disabled -bool false
+defaults write -g NSScrollViewRubberbanding -int 0
+defaults write -g ApplePressAndHoldEnabled -bool false 
+
+# Disable Notification Center 
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
+
+# Disable Spotlight indexing for external volumes
+sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
+## Fine tune Spotlight indexing order and search results
+#defaults write com.apple.spotlight orderedItems -array \
+#  '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+#  '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+#  '{"enabled" = 1;"name" = "DIRECTORIES";}' \
+#  '{"enabled" = 1;"name" = "PDF";}' \
+#  '{"enabled" = 1;"name" = "FONTS";}' \
+#  '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+#  '{"enabled" = 0;"name" = "MESSAGES";}' \
+#  '{"enabled" = 0;"name" = "CONTACT";}' \
+#  '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+#  '{"enabled" = 0;"name" = "IMAGES";}' \
+#  '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+#  '{"enabled" = 0;"name" = "MUSIC";}' \
+#  '{"enabled" = 0;"name" = "MOVIES";}' \
+#  '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+#  '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+#  '{"enabled" = 0;"name" = "SOURCE";}' \
+#  '{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
+#  '{"enabled" = 0;"name" = "MENU_OTHER";}' \
+#  '{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
+#  '{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
+#  '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+#  '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+# Reload Spotlight and rebuild indexes
+killall mds > /dev/null 2>&1
+sudo mdutil -i on / > /dev/null
+sudo mdutil -E / > /dev/null
+
+## Disable Launchpad fade-in animation
+#defaults write com.apple.dock springboard-show-duration -int 0
+#defaults write com.apple.dock springboard-hide-duration -int 0
+
+# Make the dock slightly more transparent
+defaults write com.apple.dock hide-mirror -bool true
+
+# Disable new window pop-in animation
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool NO
