be316d14282309c6afc4cceb51e6ca94590d3e2f
david cochran <about.trout@gmail.com>
2020-01-11 21:39:28 -0800

    add my tmux themes

--- link.sh
+++ link.sh
@@ -5,9 +5,10 @@ ln -s $PWD/shell/.bash_profile ~/.bash_profile
 ln -s $PWD/shell/.bashrc ~/.bashrc
 ln -s $PWD/shell/.inputrc ~/.inputrc
 
-rm -i ~/.tmux.conf
+rm -ri ~/.{tmux,tmux.conf}
+ln -s $PWD/tmux ~/.tmux
 ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
 
-rm -ri ~/{vim,.vimrc}
+rm -ri ~/.{vim,vimrc}
 ln -s $PWD/vim ~/.vim
 ln -s $PWD/vim/.vimrc ~/.vimrc
