b64574dd37c63f0f9e570388099492f4e88b1cde
david cochran <about.trout@gmail.com>
2020-01-11 20:22:14 -0800

    add setup script

---
+++ link.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -ri ~/.{bash_profile,bashrc,inputrc}
+ln -s $PWD/shell/.bash_profile ~/.bash_profile
+ln -s $PWD/shell/.bashrc ~/.bashrc
+ln -s $PWD/shell/.inputrc ~/.inputrc
+
+rm -i ~/.tmux.conf
+ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
+
+rm -ri ~/{vim,.vimrc}
+ln -s $PWD/vim ~/.vim
+ln -s $PWD/vim/.vimrc ~/.vimrc
